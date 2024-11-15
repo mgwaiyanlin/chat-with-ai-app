46f74a6709896263231cc14090c3be1cfd8fdaeb
mgwaiyanlin <waiyanlinn.netuser@gmail.com>
2024-11-15 16:42:58 +0630

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import StartPage from "./components/StartPage"
+
+function App() {
+
+  return (
+    <div className="container">
+      <StartPage />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/StartPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './StartPage.css' 
+
+const StartPage = () => {
+  return (
+    <div className='start-page'>
+      <h1 className='header1'>Let's chat with AI</h1>
+      <button className='start-page-btn'>Start</button>
+    </div>
+  )
+}
+
+export default StartPage
